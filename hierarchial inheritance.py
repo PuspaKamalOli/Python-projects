7373deed8c7e44101e5547cc2e57d012713c7d72
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2023-04-08 12:55:20 +0545

    Add files via upload

---
+++ hierarchial inheritance.py
@@ -0,0 +1,11 @@
+class Animal:
+    def move(self):
+        print("Animal moves")
+
+class Cat(Animal):
+    def meow(self):
+        print("Cat meows")
+
+class Lion(Animal):
+    def roar(self):
+        print("Lion roars")
