10709d59826c7b23b91bd1808367051479cc8ecb
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2023-04-08 13:51:42 +0545

    Add files via upload

---
+++ static methods.py
@@ -0,0 +1,8 @@
+class Math:
+    @staticmethod
+    def square(x):
+        return x * x
+
+# Calling static method
+result = Math.square(5)
+print("Square of 5:", result)  # Output: Square of 5: 25
