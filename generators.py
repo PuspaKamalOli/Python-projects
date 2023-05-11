b0b0bb2ec0c1a5f00da80334e237881f1d1f325e
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2023-05-11 10:48:32 +0545

    Update generators.py

--- generators.py
+++ generators.py
@@ -1,7 +1,11 @@
 
-#In Python, a generator is a special type of iterable, which allows you to iterate over a sequence of values without actually creating the entire sequence in memory at once. Instead, a generator generates each value on-the-fly as you iterate over it.
+#In Python, a generator is a special type of iterable, which allows you to iterate 
+#over a sequence of values without actually creating the entire sequence in memory at once. Instead,
+#a generator generates each value on-the-fly as you iterate over it.
 
-#Generators are defined using the yield keyword, which is similar to return but instead of stopping the function and returning a value, it suspends the function and returns a generator object that can be used to iterate over the values produced by the generator.#
+#Generators are defined using the yield keyword, which is similar to return but instead of 
+#stopping the function and returning a value, 
+#it suspends the function and returns a generator object that can be used to iterate over the values produced by the generator.#
 
 def number_sequence(n):
     for i in range(n):
